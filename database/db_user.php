43784680135df3c09c6438a6c0839ae1aa7a776c
Álvaro <joaoalvaro.ferreira@hotmail.com>
2018-12-09 03:22:33 +0000

    pequeno update, hashing não está a dar, assim funciona mas está sem hashing

--- database/db_user.php
+++ database/db_user.php
@@ -5,7 +5,7 @@
   function checkPassword($username, $password) {
     $db = Database::getInstance()->db();
     $stmt = $db->prepare('SELECT * FROM users WHERE username = ? AND passw = ?');
-    $stmt->execute(array($username, password_hash($password, PASSWORD_DEFAULT)));
+    $stmt->execute(array($username, $password));
     return $stmt->fetch()?true:false; // return true if a line exists
   }
 
@@ -21,7 +21,7 @@
       
   
     $stmt = $db->prepare('INSERT INTO users VALUES(?, ?, ?)');
-    $stmt->execute(array($username, password_hash($password, PASSWORD_DEFAULT),$date));
+    $stmt->execute(array($username, $password,$date));
       
       return true;
   }
